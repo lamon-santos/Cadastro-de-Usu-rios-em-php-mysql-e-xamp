09481a603e13a8bf5be6f92b5faee62292b47797
Lamon Santos <developerdartflutter@gmail.com>
2025-01-06 10:20:22 -0300

    dando continuidade ao código

--- validar.php
+++ validar.php
@@ -1,4 +1,5 @@
 <?php
+    require_once('conexao.php'); // Inclui o arquivo conexao.php na página atual para que possamos utilizar a classe db
     $usuario = $_POST["usuario"];
     $email = $_POST["email"];
     $senha = $_POST["senha"];
