570be29ef22864458cfd69e2a0fad73033194111
Lamon Santos <developerdartflutter@gmail.com>
2025-01-06 10:34:58 -0300

    finalização do código

--- validar.php
+++ validar.php
@@ -3,4 +3,13 @@
     $usuario = $_POST["usuario"];
     $email = $_POST["email"];
     $senha = $_POST["senha"];
+    $objDb = new db(); // Instancia a classe db em um objeto chamado objDb
+    $link = $objDb->conecta_mysql(); // Conecta ao banco de dados
+    $sql = "INSERT INTO usuarios(usuario, email, senha) VALUES ('$usuario', '$email', '$senha')"; // Cria a query SQL
+    // Executa a query SQL
+    if(mysqli_query($link, $sql)){
+        echo 'Usuário registrado com sucesso!';
+    }else{
+        echo 'Erro ao registrar o usuário!';
+    }
 ?>
